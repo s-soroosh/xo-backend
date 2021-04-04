a3044df3774d42857e85b13d1c70aa855b64f38e
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2021-04-04 18:06:37 +0200

    add commands.sh

--- commands.sh
+++ commands.sh
@@ -2,15 +2,15 @@ URL=https://xo-backend-soroosh.fandogh.cloud
 URL=http://localhost:1337
 #register
 
-curl http://localhost:1337/auth/local/register -H "Content-Type: application/json" -d '{"username":"test1","email":"myemail@gmail.com", "password":"123456"}'
+curl $URL/auth/local/register -H "Content-Type: application/json" -d '{"username":"test1","email":"myemail@gmail.com", "password":"123456"}'
 
 #login
 
-curl http://localhost:1337/auth/local -H "Content-Type: application/json" -d '{"identifier":"myemail@gmail.com", "password":"123456"}'
+curl $URL/auth/local -H "Content-Type: application/json" -d '{"identifier":"myemail@gmail.com", "password":"123456"}'
 
 
 # create game
 curl $URL/games -H "Content-Type: application/json" -d '{"size":3}' -H "Authorization: Bearer ${TOKEN}"
 
 # join game
-curl http://localhost:1337/games/join -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}"
+curl $URL/games/join -H "Content-Type: application/json"  -H "Authorization: Bearer ${TOKEN}"
