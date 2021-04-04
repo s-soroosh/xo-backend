0cbea18c883f7346a93eccb9b0fc50fc4a88cc2b
Soroosh Sarabadani <soroosh.sarabadani@zalando.de>
2021-04-04 15:54:49 +0200

    implement join

--- api/game/controllers/game.js
+++ api/game/controllers/game.js
@@ -1,8 +1,23 @@
 'use strict';
+const Boom = require('boom')
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  async join(ctx) {
+    const id = ctx.params.id
+    const game = await strapi.query("game").findOne({id})
+    const currentUser = ctx.state.user
+    if (game.player2 === null && game.player1.id !== currentUser.id) {
+      game.player2 = currentUser
+      strapi.query("game").update({id}, game)
+    } else {
+      throw Boom.badRequest('Player cannot join this game')
+    }
+
+    return ctx
+  }
+};
